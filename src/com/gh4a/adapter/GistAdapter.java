11a6f5f545c4b11af34fb9cc17b40b4ba26413af
slapperwan <slapperwan@gmail.com>
2011-03-20 14:38:37 +0800

    Add license

--- src/com/gh4a/adapter/GistAdapter.java
+++ src/com/gh4a/adapter/GistAdapter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Azwan Adli Abdullah
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.gh4a.adapter;
 
 import java.util.List;
@@ -12,12 +27,24 @@ import com.gh4a.R;
 import com.gh4a.utils.StringUtils;
 import com.github.api.v2.schema.Gist;
 
+/**
+ * The Gist adapter.
+ */
 public class GistAdapter  extends RootAdapter<Gist> {
 
+    /**
+     * Instantiates a new gist adapter.
+     *
+     * @param context the context
+     * @param objects the objects
+     */
     public GistAdapter(Context context, List<Gist> objects) {
         super(context, objects);
     }
 
+    /* (non-Javadoc)
+     * @see com.gh4a.adapter.RootAdapter#doGetView(int, android.view.View, android.view.ViewGroup)
+     */
     @Override
     public View doGetView(int position, View convertView, ViewGroup parent) {
         View v = convertView;
