679e3c6920406a23531a59d9fad6e5c2fd2e1db9
Azwan Adli <slapperwan@gmail.com>
2012-06-17 23:10:05 +0800

    Remove github job and enable public timeline

--- src/com/gh4a/TimelineActivity.java
+++ src/com/gh4a/TimelineActivity.java
@@ -17,39 +17,30 @@ package com.gh4a;
 
 import java.util.List;
 
+import org.eclipse.egit.github.core.event.Event;
+import org.eclipse.egit.github.core.service.EventService;
+
+import com.gh4a.holder.BreadCrumbHolder;
+
 
 
 public class TimelineActivity extends UserFeedActivity {
 
     @Override
     public List getFeeds() {
-        // TODO Auto-generated method stub
-        return null;
+        EventService eventService = new EventService();
+        return (List<Event>) eventService.pagePublicEvents().next();    
     }
 
-//    @Override
-//    public List<UserFeed> getFeeds() {
-//        try {
-//            GitHubServiceFactory factory = GitHubServiceFactory.newInstance();
-//            FeedService feedService = factory.createFeedService();
-//            return feedService.getPublicTimelineFeedJson();
-//        }
-//        catch (GitHubException e) {
-//            Log.e(Constants.LOG_TAG, e.getMessage(), e);
-//            showError();
-//            return null;
-//        }
-//    }
-//    
-//    @Override
-//    protected void setBreadCrumb() {
-//        BreadCrumbHolder[] breadCrumbHolders = new BreadCrumbHolder[1];
-//
-//        BreadCrumbHolder b = new BreadCrumbHolder();
-//        b.setLabel(getResources().getString(R.string.explore));
-//        b.setTag(Constants.EXPLORE);
-//        breadCrumbHolders[0] = b;
-//        
-//        createBreadcrumb(getResources().getStringArray(R.array.explore_item)[0], breadCrumbHolders);
-//    }
+    @Override
+    protected void setBreadCrumb() {
+        BreadCrumbHolder[] breadCrumbHolders = new BreadCrumbHolder[1];
+
+        BreadCrumbHolder b = new BreadCrumbHolder();
+        b.setLabel(getResources().getString(R.string.explore));
+        b.setTag(Constants.EXPLORE);
+        breadCrumbHolders[0] = b;
+        
+        createBreadcrumb(getResources().getStringArray(R.array.explore_item)[0], breadCrumbHolders);
+    }
 }
