751b1e95d9b0f2c4b5fbf939fbd922c97f114bf5
slapperwan <slapperwan@gmail.com>
2014-04-09 11:17:05 +0800

    Fix 2fa for first time user login

--- src/com/gh4a/ClientForAuthorization.java
+++ src/com/gh4a/ClientForAuthorization.java
@@ -1,11 +1,14 @@
 package com.gh4a;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.client.GitHubRequest;
 import org.eclipse.egit.github.core.client.GitHubResponse;
+
+import android.util.Log;
 
 import com.gh4a.utils.StringUtils;
 
@@ -39,16 +42,56 @@ public class ClientForAuthorization extends GitHubClient {
             if (!StringUtils.isBlank(otpHeader)
                     && otpHeader.contains("required")) {
 
-                String twoFactorAuthType = null;
-                if (otpHeader.contains("app")) {
-                    twoFactorAuthType = "app";
-                } else if (otpHeader.contains("sms")) {
-                    twoFactorAuthType = "sms";
-                }
-
-                throw new TwoFactorAuthException(e, twoFactorAuthType);
+                throw getTwoFactorAuthException(e, otpHeader);
             }
             throw e;
         }
+    }
+
+    @Override
+    public <V> V post(String uri, Object params, Type type) throws IOException {
+        HttpURLConnection conn = createPost(uri);
+        try {
+            Log.d("XXXX", "+++++ " + otpCode);
+            if (!StringUtils.isBlank(otpCode)) {
+                conn.setRequestProperty("X-GitHub-OTP", otpCode);
+            }
+            return sendJson(conn, params, type);
+        } catch (IOException e) {
+            String otpHeader = conn.getHeaderField("X-GitHub-OTP");
+            if (!StringUtils.isBlank(otpHeader)
+                    && otpHeader.contains("required")) {
+
+                throw getTwoFactorAuthException(e, otpHeader);
+            }
+            throw e;
+        }
+    }
+    
+    private TwoFactorAuthException getTwoFactorAuthException(IOException e, String otpHeader) {
+        String twoFactorAuthType = null;
+        if (otpHeader.contains("app")) {
+            twoFactorAuthType = "app";
+        } else if (otpHeader.contains("sms")) {
+            twoFactorAuthType = "sms";
+        }
+
+        return new TwoFactorAuthException(e, twoFactorAuthType);
+    }
+    
+    private <V> V sendJson(final HttpURLConnection request,
+            final Object params, final Type type) throws IOException {
+        sendParams(request, params);
+        final int code = request.getResponseCode();
+        updateRateLimits(request);
+        if (isOk(code))
+            if (type != null)
+                return parseJson(getStream(request), type);
+            else
+                return null;
+        if (isEmpty(code))
+            return null;
+        throw createException(getStream(request), code,
+                request.getResponseMessage());
     }
 }
