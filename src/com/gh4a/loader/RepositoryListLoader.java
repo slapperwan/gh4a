a11d1adba6180a01ed85acfaa057ad6c7f80fd9d
slapperwan <slapperwan@gmail.com>
2014-05-14 18:17:18 +0800

    List private org repo

--- src/com/gh4a/loader/RepositoryListLoader.java
+++ src/com/gh4a/loader/RepositoryListLoader.java
@@ -2,6 +2,7 @@ package com.gh4a.loader;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.egit.github.core.Repository;
@@ -9,12 +10,14 @@ import org.eclipse.egit.github.core.service.RepositoryService;
 
 import android.content.Context;
 
+import com.gh4a.Constants;
 import com.gh4a.Gh4Application;
 
 public class RepositoryListLoader extends BaseLoader<Collection<Repository>> {
     private String mLogin;
     private Map<String, String> mFilterData;
     private int mSize;
+    private String mUserType;
 
     public RepositoryListLoader(Context context, String login, String userType,
             Map<String, String> filterData, int size) {
@@ -22,6 +25,7 @@ public class RepositoryListLoader extends BaseLoader<Collection<Repository>> {
         this.mLogin = login;
         this.mFilterData = filterData;
         this.mSize = size;
+        this.mUserType = userType;
     }
 
     @Override
@@ -33,6 +37,12 @@ public class RepositoryListLoader extends BaseLoader<Collection<Repository>> {
                 return repoService.pageRepositories(mFilterData, mSize).next();
             } else {
                 return repoService.getRepositories(mFilterData);
+            }
+        } else if (Constants.User.TYPE_ORG.equals(mUserType)) {
+            if (mSize > 0) {
+                return (List<Repository>) repoService.pageOrgRepositories(mLogin, mFilterData, mSize).next();
+            } else {
+                return repoService.getOrgRepositories(mLogin, mFilterData);
             }
         } else {
             if (mSize > 0) {
