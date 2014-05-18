ba6119be4b6fb43fb8562cf9b8f0dfd11b938a88
Gautam Korlam <kageiit@gmail.com>
2014-05-18 02:46:12 -0700

    Imrpoved Code/Markdown display according to theme

---
+++ src/com/gh4a/utils/ThemeUtils.java
@@ -0,0 +1,21 @@
+package com.gh4a.utils;
+
+import android.graphics.Color;
+
+import com.gh4a.R;
+
+public class ThemeUtils {
+    public static final int LIGHT_BACKGROUND_COLOR = Color.parseColor("#F7F7F9");
+    private static final int DARK_BACKGROUND_COLOR = Color.parseColor("#111111");
+
+    private static final String DARK_CSS_THEME = "dark";
+    private static final String LIGHT_CSS_THEME = "light";
+
+    public static String getCssTheme(int theme) {
+        return theme == R.style.DefaultTheme ? DARK_CSS_THEME : LIGHT_CSS_THEME;
+    }
+
+    public static int getWebViewBackgroundColor(int theme) {
+        return theme == R.style.DefaultTheme ? DARK_BACKGROUND_COLOR : LIGHT_BACKGROUND_COLOR;
+    }
+}
