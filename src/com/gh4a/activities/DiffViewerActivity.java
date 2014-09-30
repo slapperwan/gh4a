dde378f7c324bc6f87f1e197a4fa758d1708b379
Danny Baumann <dannybaumann@web.de>
2014-09-30 08:26:32 +0200

    Fix 'View file at...'.
    The commits API expects commit SHAs, not tree SHAs.

--- src/com/gh4a/activities/DiffViewerActivity.java
+++ src/com/gh4a/activities/DiffViewerActivity.java
@@ -168,8 +168,7 @@ public abstract class DiffViewerActivity extends WebViewerActivity implements
                 startActivity(shareIntent);
                 return true;
             case MENU_ITEM_VIEW:
-                IntentUtils.openFileViewerActivity(this, mRepoOwner, mRepoName,
-                        mRef, mPath);
+                IntentUtils.openFileViewerActivity(this, mRepoOwner, mRepoName, mSha, mPath);
                 return true;
         }
         return super.onOptionsItemSelected(item);
