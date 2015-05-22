slapperwan/gh4a

commit bcea65bb8b23d72b935682e00ef83d5bd4d9cea9

Don't suppress activity recreations due to configuration changes.
This prevents e.g. having per-orientation layouts. The loaded data is
retained during the recreations in the loader instances.

src/com/gh4a/fragment/PagedDataBaseFragment.java
@@ -40,7 +40,6 @@ import java.util.Collection;
 public abstract class PagedDataBaseFragment<T> extends LoadingListFragmentBase implements
         LoaderManager.LoaderCallbacks<Collection<T>>, OnScrollListener {
     private RootAdapter<T> mAdapter;
-    private boolean mLoadMore;
     private boolean mIsLoadCompleted;
     private TextView mLoadingView;
     private String mCurrentFilter;
@@ -81,26 +80,20 @@ public abstract class PagedDataBaseFragment<T> extends LoadingListFragmentBase i
     }
 
     public void refresh() {
-        mLoadMore = false;
         if (getListView() != null) {
             setListShown(false);
             getLoaderManager().getLoader(0).onContentChanged();
         }
     }
 
-    private void fillData(Collection<T> data) {
+    private void fillData(PageIteratorLoader<T> loader, Collection<T> data) {
         ListView listView = getListView();
-        if (data == null || data.isEmpty()) {
+        if (!loader.hasMoreData()) {
             listView.removeFooterView(mLoadingView);
-            return;
-        }
-        if (getListView().getFooterViewsCount() == 0) {
+        } else if (getListView().getFooterViewsCount() == 0) {
             listView.addFooterView(mLoadingView);
-            setListAdapter(mAdapter);
         }
-        if (!mLoadMore) {
-            mAdapter.clear();
-        }
+        mAdapter.clear();
         onAddData(mAdapter, data);
     }
 
@@ -115,7 +108,7 @@ public abstract class PagedDataBaseFragment<T> extends LoadingListFragmentBase i
 
     @Override
     public void onLoadFinished(Loader<Collection<T>> loader, Collection<T> events) {
-        fillData(events);
+        fillData((PageIteratorLoader<T>) loader, events);
         mIsLoadCompleted = true;
         setListShown(true);
         invalidateOptionsMenu();
@@ -131,10 +124,11 @@ public abstract class PagedDataBaseFragment<T> extends LoadingListFragmentBase i
 
     @Override
     public void onScroll(AbsListView view, int firstVisible, int visibleCount, int totalCount) {
-        boolean loadMore = firstVisible + visibleCount >= totalCount;
+        boolean loadMore = firstVisible > 0 && visibleCount > 0 &&
+                getListView().getFooterViewsCount() != 0 &&
+                firstVisible + visibleCount >= totalCount;
 
         if (loadMore && mIsLoadCompleted) {
-            mLoadMore = true;
             mIsLoadCompleted = false;
             getLoaderManager().getLoader(0).forceLoad();
         }

AndroidManifest.xml
@@ -24,139 +24,58 @@
                     android:name="android.intent.category.LAUNCHER" />
             </intent-filter>
         </activity>
+
         <activity-alias
             android:name=".Github4AndroidActivity"
             android:label="@string/app_name"
             android:targetActivity=".activities.Github4AndroidActivity"/>
-        <activity
-            android:name=".activities.BlogActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.BlogListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.BookmarkListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.CollaboratorListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.CommitActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.CommitHistoryActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.CompareActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.ContributorListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.DownloadsActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.EditCommitCommentActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.EditIssueCommentActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.EditPullRequestCommentActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.FileViewerActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.ForkListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.CommitDiffViewerActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.FollowerFollowingListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.GistActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.GistListActivity"
-            android:configChanges="orientation|screenSize"/>
-        <activity
-            android:name=".activities.GistViewerActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.IssueActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.IssueEditActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.IssueLabelListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.IssueListActivity"
-            android:configChanges="orientation|screenSize"
-            android:uiOptions="splitActionBarWhenNarrow" />
-        <activity
-            android:name=".activities.IssueListMineActivity"
-            android:configChanges="orientation|screenSize"
-            android:uiOptions="splitActionBarWhenNarrow" />
-        <activity
-            android:name=".activities.IssueMilestoneEditActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.IssueMilestoneListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.OrganizationMemberListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.PullRequestActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.PullRequestListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.PullRequestDiffViewerActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.ReleaseInfoActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.ReleaseListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.RepositoryActivity"
-            android:configChanges="orientation|screenSize"
-            android:uiOptions="splitActionBarWhenNarrow" />
-        <activity
-            android:name=".activities.RepositoryListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.SearchActivity"
-            android:configChanges="orientation|screenSize" />
+
+        <activity android:name=".activities.BlogActivity" />
+        <activity android:name=".activities.BlogListActivity" />
+        <activity android:name=".activities.BookmarkListActivity" />
+        <activity android:name=".activities.CollaboratorListActivity" />
+        <activity android:name=".activities.CommitActivity" />
+        <activity android:name=".activities.CommitDiffViewerActivity" />
+        <activity android:name=".activities.CommitHistoryActivity" />
+        <activity android:name=".activities.CompareActivity" />
+        <activity android:name=".activities.ContributorListActivity" />
+        <activity android:name=".activities.DownloadsActivity" />
+        <activity android:name=".activities.EditCommitCommentActivity" />
+        <activity android:name=".activities.EditIssueCommentActivity" />
+        <activity android:name=".activities.EditPullRequestCommentActivity" />
+        <activity android:name=".activities.FileViewerActivity" />
+        <activity android:name=".activities.ForkListActivity" />
+        <activity android:name=".activities.FollowerFollowingListActivity" />
+        <activity android:name=".activities.GistActivity" />
+        <activity android:name=".activities.GistListActivity" />
+        <activity android:name=".activities.GistViewerActivity" />
+        <activity android:name=".activities.IssueActivity" />
+        <activity android:name=".activities.IssueEditActivity" />
+        <activity android:name=".activities.IssueLabelListActivity" />
+        <activity android:name=".activities.IssueListActivity" />
+        <activity android:name=".activities.IssueListMineActivity" />
+        <activity android:name=".activities.IssueMilestoneEditActivity" />
+        <activity android:name=".activities.IssueMilestoneListActivity" />
+        <activity android:name=".activities.OrganizationMemberListActivity" />
+        <activity android:name=".activities.PullRequestActivity" />
+        <activity android:name=".activities.PullRequestListActivity" />
+        <activity android:name=".activities.PullRequestDiffViewerActivity" />
+        <activity android:name=".activities.ReleaseInfoActivity" />
+        <activity android:name=".activities.ReleaseListActivity" />
+        <activity android:name=".activities.RepositoryActivity" />
+        <activity android:name=".activities.RepositoryListActivity" />
+        <activity android:name=".activities.SearchActivity" />
+        <activity android:name=".activities.TimelineActivity" />
+        <activity android:name=".activities.TrendingActivity" />
+        <activity android:name=".activities.UserActivity" />
+        <activity android:name=".activities.WatcherListActivity" />
+        <activity android:name=".activities.WikiActivity" />
+        <activity android:name=".activities.WikiListActivity" />
+
         <activity
             android:name=".activities.SettingsActivity"
             android:title="@string/settings" />
-        <activity
-            android:name=".activities.TimelineActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.TrendingActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.UserActivity"
-            android:configChanges="orientation|screenSize"/>
-        <activity
-            android:name=".activities.WatcherListActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.WikiActivity"
-            android:configChanges="orientation|screenSize" />
-        <activity
-            android:name=".activities.WikiListActivity"
-            android:configChanges="orientation|screenSize" />
+
         <activity android:name="BrowseFilter">
             <intent-filter>
                 <action
@@ -179,10 +98,12 @@
                     android:scheme="http" />
             </intent-filter>
         </activity>
+
         <provider
             android:name=".db.BookmarksProvider"
             android:authorities="com.gh4a"
             android:exported="false" />
+
         <receiver
             android:name="EventReceiver">
             <intent-filter>

src/com/gh4a/activities/IssueEditActivity.java
@@ -58,7 +58,9 @@ import org.eclipse.egit.github.core.service.IssueService;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class IssueEditActivity extends LoadingFragmentActivity implements OnClickListener {
     private String mRepoOwner;
@@ -66,6 +68,7 @@ public class IssueEditActivity extends LoadingFragmentActivity implements OnClic
     private int mIssueNumber;
 
     private List<Label> mSelectedLabels;
+    private boolean mLabelSelectionChanged;
     private Milestone mSelectedMilestone;
     private User mSelectedAssignee;
     private List<Milestone> mAllMilestone;
@@ -75,6 +78,9 @@ public class IssueEditActivity extends LoadingFragmentActivity implements OnClic
     private ProgressDialog mProgressDialog;
     private TextView mTvSelectedMilestone;
     private TextView mTvSelectedAssignee;
+    private Map<Label, TextView> mLabelViewMap;
+
+    private static final String STATE_KEY_LABELS = "selected_labels";
 
     private LoaderCallbacks<List<Label>> mLabelCallback = new LoaderCallbacks<List<Label>>() {
         @Override
@@ -192,10 +198,24 @@ public class IssueEditActivity extends LoadingFragmentActivity implements OnClic
             getSupportLoaderManager().initLoader(3, null, mIssueCallback);
         }
         setContentShown(!isInEditMode());
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(STATE_KEY_LABELS)) {
+            mSelectedLabels = (ArrayList<Label>) savedInstanceState.getSerializable(STATE_KEY_LABELS);
+            mLabelSelectionChanged = true;
+            applyLabelSelection();
+        }
     }
 
     private boolean isInEditMode() {
         return mIssueNumber != 0;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mLabelSelectionChanged) {
+            outState.putSerializable(STATE_KEY_LABELS, new ArrayList<Label>(mSelectedLabels));
+        }
     }
 
     @Override
@@ -361,6 +381,8 @@ public class IssueEditActivity extends LoadingFragmentActivity implements OnClic
     public void fillLabels(List<Label> labels) {
         LinearLayout labelLayout = (LinearLayout) findViewById(R.id.ll_labels);
 
+        mLabelViewMap = new HashMap<Label, TextView>();
+
         for (final Label label : labels) {
             final View rowView = getLayoutInflater().inflate(R.layout.row_issue_create_label, null);
             View viewColor = rowView.findViewById(R.id.view_color);
@@ -371,45 +393,48 @@ public class IssueEditActivity extends LoadingFragmentActivity implements OnClic
             tvLabel.setOnClickListener(this);
             tvLabel.setTag(label);
 
-            if (isInEditMode()) {
-                handleLabelClick(tvLabel, label, mSelectedLabels.contains(label));
-            }
-
             labelLayout.addView(rowView);
+            mLabelViewMap.put(label, tvLabel);
         }
+        applyLabelSelection();
     }
 
     @Override
     public void onClick(View view) {
         if (view.getTag() instanceof Label) {
             Label label = (Label) view.getTag();
-            TextView tvLabel = (TextView) view;
 
+            mLabelSelectionChanged = true;
             if (mSelectedLabels.contains(label)) {
                 mSelectedLabels.remove(label);
-                handleLabelClick(tvLabel, label, false);
             } else {
                 mSelectedLabels.add(label);
-                handleLabelClick(tvLabel, label, true);
             }
+
+            applyLabelSelection();
         }
     }
 
-    private void handleLabelClick(TextView tvLabel, Label label, boolean select) {
-        Gh4Application app = Gh4Application.get(this);
-        if (!select) {
-            mSelectedLabels.remove(label);
-            tvLabel.setTypeface(tvLabel.getTypeface(), 0);
-            tvLabel.setBackgroundColor(0);
-            tvLabel.setTextColor(getResources().getColor(Gh4Application.THEME != R.style.LightTheme
-                    ? R.color.label_fg_light : R.color.label_fg_dark));
-        } else {
-            int color = Color.parseColor("#" + label.getColor());
+    private void applyLabelSelection() {
+        if (mLabelViewMap == null) {
+            return;
+        }
 
-            mSelectedLabels.add(label);
-            tvLabel.setTypeface(tvLabel.getTypeface(), Typeface.BOLD);
-            tvLabel.setBackgroundColor(color);
-            tvLabel.setTextColor(UiUtils.textColorForBackground(this, color));
+        for (Map.Entry<Label, TextView> entry : mLabelViewMap.entrySet()) {
+            Label label = entry.getKey();
+            TextView view = entry.getValue();
+
+            if (mSelectedLabels.contains(label)) {
+                int color = Color.parseColor("#" + label.getColor());
+                view.setTypeface(view.getTypeface(), Typeface.BOLD);
+                view.setBackgroundColor(color);
+                view.setTextColor(UiUtils.textColorForBackground(this, color));
+            } else {
+                view.setTypeface(view.getTypeface(), 0);
+                view.setBackgroundColor(0);
+                view.setTextColor(getResources().getColor(Gh4Application.THEME != R.style.LightTheme
+                        ? R.color.label_fg_light : R.color.label_fg_dark));
+            }
         }
     }
 
@@ -420,8 +445,11 @@ public class IssueEditActivity extends LoadingFragmentActivity implements OnClic
         EditText etDesc = (EditText) findViewById(R.id.et_desc);
         etDesc.setText(mEditIssue.getBody());
 
-        mSelectedLabels.clear();
-        mSelectedLabels.addAll(mEditIssue.getLabels());
+        if (!mLabelSelectionChanged) {
+            mSelectedLabels.clear();
+            mSelectedLabels.addAll(mEditIssue.getLabels());
+            applyLabelSelection();
+        }
 
         mSelectedMilestone = mEditIssue.getMilestone();
         mSelectedAssignee = mEditIssue.getAssignee();

src/com/gh4a/activities/IssueLabelListActivity.java
@@ -60,12 +60,15 @@ public class IssueLabelListActivity extends LoadingFragmentActivity implements
     private String mRepoOwner;
     private String mRepoName;
     private EditActionMode mActionMode;
-    private Label mAddedLabel;
-    private boolean mShouldStartAdding;
+    private IssueLabelAdapter.EditableLabel mAddedLabel;
 
     private FloatingActionButton mFab;
     private ShowHideOnScroll mFabListener;
     private ListView mListView;
+    private IssueLabelAdapter mAdapter;
+
+    private static final String STATE_KEY_ADDED_LABEL = "added_label";
+    private static final String STATE_KEY_EDITING_LABEL = "editing_label";
 
     private LoaderCallbacks<List<Label>> mLabelCallback = new LoaderCallbacks<List<Label>>() {
         @Override
@@ -78,7 +81,9 @@ public class IssueLabelListActivity extends LoadingFragmentActivity implements
             UiUtils.hideImeForView(getCurrentFocus());
             if (success) {
                 mAdapter.clear();
-                mAdapter.addAll(result.getData());
+                for (Label label : result.getData()) {
+                    mAdapter.add(new IssueLabelAdapter.EditableLabel(label));
+                }
                 mAdapter.notifyDataSetChanged();
             }
             setContentEmpty(!success);
@@ -86,33 +91,7 @@ public class IssueLabelListActivity extends LoadingFragmentActivity implements
         }
     };
 
-    private IssueLabelAdapter mAdapter = new IssueLabelAdapter(this) {
-        @Override
-        protected void bindView(View view, Label label) {
-            ViewHolder holder = (ViewHolder) view.getTag();
-
-            super.bindView(view, label);
-
-            if (label == mAddedLabel && mShouldStartAdding) {
-                holder.editor.setHint(R.string.issue_label_new);
-                mActionMode = new EditActionMode(label, holder.editor);
-                startSupportActionMode(mActionMode);
-                updateFabVisibility();
-                mShouldStartAdding = false;
-            } else {
-                holder.editor.setHint(null);
-            }
-
-            Label editingLabel = mActionMode != null ? mActionMode.mLabel : null;
-            if (label.equals(editingLabel)) {
-                setExpanded(view, true);
-                mActionMode.setEditor(holder.editor);
-            } else {
-                setExpanded(view, false);
-            }
-        }
-    };
-
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -126,6 +105,12 @@ public class IssueLabelListActivity extends LoadingFragmentActivity implements
         setContentView(R.layout.issue_label_list);
         setContentShown(false);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.issue_labels);
+        actionBar.setSubtitle(mRepoOwner + "/" + mRepoName);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mAdapter = new IssueLabelAdapter(this);
         mListView = (ListView) findViewById(R.id.main_content);
         mListView.setAdapter(mAdapter);
         mListView.setOnItemClickListener(this);
@@ -135,22 +120,47 @@ public class IssueLabelListActivity extends LoadingFragmentActivity implements
         mFabListener = new ShowHideOnScroll(mFab);
         updateFabVisibility();
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle(R.string.issue_labels);
-        actionBar.setSubtitle(mRepoOwner + "/" + mRepoName);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
         getSupportLoaderManager().initLoader(0, null, mLabelCallback);
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(STATE_KEY_ADDED_LABEL)) {
+                mAddedLabel = (IssueLabelAdapter.EditableLabel)
+                        savedInstanceState.getSerializable(STATE_KEY_EDITING_LABEL);
+                mAdapter.add(mAddedLabel);
+                startEditing(mAddedLabel);
+            } else if (savedInstanceState.containsKey(STATE_KEY_EDITING_LABEL)) {
+                IssueLabelAdapter.EditableLabel label = (IssueLabelAdapter.EditableLabel)
+                        savedInstanceState.getSerializable(STATE_KEY_EDITING_LABEL);
+                int count = mAdapter.getCount();
+                for (int i = 0; i < count; i++) {
+                    IssueLabelAdapter.EditableLabel item = mAdapter.getItem(i);
+                    if (item.getName().equals(label.getName())) {
+                        item.editedName = label.editedName;
+                        item.editedColor = label.editedColor;
+                        startEditing(item);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mActionMode != null) {
+            if (mAddedLabel != null) {
+                outState.putSerializable(STATE_KEY_ADDED_LABEL, mAddedLabel);
+            } else {
+                outState.putSerializable(STATE_KEY_EDITING_LABEL, mActionMode.mLabel);
+            }
+        }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         if (mActionMode == null) {
-            mActionMode = new EditActionMode(mAdapter.getItem(position),
-                    (EditText) view.findViewById(R.id.et_label));
-            mAdapter.setExpanded(view, true);
-            startSupportActionMode(mActionMode);
-            updateFabVisibility();
+            startEditing(mAdapter.getItem(position));
         }
     }
 
@@ -163,12 +173,17 @@ public class IssueLabelListActivity extends LoadingFragmentActivity implements
     @Override
     public void onClick(View view) {
         if (mActionMode == null) {
-            mAddedLabel = new Label();
-            mAddedLabel.setColor("dddddd");
+            mAddedLabel = new IssueLabelAdapter.EditableLabel("dddddd");
             mAdapter.add(mAddedLabel);
-            mShouldStartAdding = true;
-            mAdapter.notifyDataSetChanged();
+            startEditing(mAddedLabel);
         }
+    }
+
+    private void startEditing(IssueLabelAdapter.EditableLabel label) {
+        mActionMode = new EditActionMode(label);
+        mAdapter.notifyDataSetChanged();
+        startSupportActionMode(mActionMode);
+        updateFabVisibility();
     }
 
     private void updateFabVisibility() {
@@ -179,25 +194,14 @@ public class IssueLabelListActivity extends LoadingFragmentActivity implements
             mListView.setOnTouchListener(null);
             mFab.setVisibility(View.GONE);
         }
-
     }
+
     private final class EditActionMode implements ActionMode.Callback {
-        private String mCurrentLabelName;
-        private EditText mEditor;
-        private Label mLabel;
+        private IssueLabelAdapter.EditableLabel mLabel;
 
-        public EditActionMode(Label label, EditText editor) {
-            mCurrentLabelName = label.getName();
-            mEditor = editor;
+        public EditActionMode(IssueLabelAdapter.EditableLabel label) {
             mLabel = label;
-
-            mEditor.setText(mCurrentLabelName);
-        }
-
-        public void setEditor(EditText editor) {
-            Editable currentText = mEditor.getText();
-            mEditor = editor;
-            mEditor.setText(currentText);
+            mLabel.isEditing = true;
         }
 
         @Override
@@ -224,23 +228,20 @@ public class IssueLabelListActivity extends LoadingFragmentActivity implements
         public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
             switch (item.getItemId()) {
             case Menu.FIRST:
-                String labelName = mEditor.getText().toString();
-                String color = (String) mEditor.getTag();
                 if (mLabel == mAddedLabel) {
-                    new AddIssueLabelTask(labelName, color).execute();
-                    mLabel = null;
+                    new AddIssueLabelTask(mLabel.editedName, mLabel.editedColor).execute();
                 } else {
-                    new EditIssueLabelTask(mCurrentLabelName, labelName, color).execute();
+                    new EditIssueLabelTask(mLabel.getName(), mLabel.editedName, mLabel.editedColor).execute();
                 }
                 break;
             case Menu.FIRST + 1:
                 new AlertDialog.Builder(IssueLabelListActivity.this)
-                        .setTitle(getString(R.string.issue_dialog_delete_title, mCurrentLabelName))
+                        .setTitle(getString(R.string.issue_dialog_delete_title, mLabel.getName()))
                         .setMessage(R.string.issue_dialog_delete_message)
                         .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int whichButton) {
-                                new DeleteIssueLabelTask(mCurrentLabelName).execute();
+                                new DeleteIssueLabelTask(mLabel.getName()).execute();
                             }
                         })
                         .setNegativeButton(R.string.cancel, null)
@@ -257,6 +258,7 @@ public class IssueLabelListActivity extends LoadingFragmentActivity implements
         @Override
         public void onDestroyActionMode(ActionMode mode) {
             mActionMode = null;
+            mLabel.isEditing = false;
             if (mLabel == mAddedLabel) {
                 mAdapter.remove(mLabel);
                 mAddedLabel = null;

commit 32fa1565f208dbc9131642ed3fde271869e79719

Preserve scroll position when adding new data to a paged list.
This got broken in bcea65bb8b23d72b935682e00ef83d5bd4d9cea9.

src/com/gh4a/fragment/PagedDataBaseFragment.java
@@ -34,6 +34,7 @@ import com.gh4a.utils.UiUtils;
 
 import org.eclipse.egit.github.core.client.PageIterator;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public abstract class PagedDataBaseFragment<T> extends LoadingListFragmentBase implements
@@ -87,8 +88,23 @@ public abstract class PagedDataBaseFragment<T> extends LoadingListFragmentBase i
         } else if (getListView().getFooterViewsCount() == 0) {
             listView.addFooterView(mLoadingView);
         }
-        mAdapter.clear();
-        onAddData(mAdapter, data);
+
+        if (mAdapter.getCount() > 0 && data.iterator().next() == mAdapter.getItem(0)) {
+            // there are common items, preserve them in order to keep the scroll position
+            ArrayList<T> newData = new ArrayList<>();
+            int index = 0, count = mAdapter.getCount();
+            for (T item : data) {
+                if (index < count && item == mAdapter.getItem(index++)) {
+                    // we already know about the item
+                    continue;
+                }
+                newData.add(item);
+            }
+            onAddData(mAdapter, newData);
+        } else {
+            mAdapter.clear();
+            onAddData(mAdapter, data);
+        }
     }
 
     protected void onAddData(RootAdapter<T> adapter, Collection<T> data) {