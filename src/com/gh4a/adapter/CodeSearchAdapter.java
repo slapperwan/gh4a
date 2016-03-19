1df7164b78a66fb378e0682dbd47ccd2a6145bbc
Danny Baumann <dannybaumann@web.de>
2016-03-19 20:25:31 +0100

    Make code search more useful by showing the matching snippets.

--- src/com/gh4a/adapter/CodeSearchAdapter.java
+++ src/com/gh4a/adapter/CodeSearchAdapter.java
@@ -1,7 +1,11 @@
 package com.gh4a.adapter;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableStringBuilder;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +15,9 @@ import com.gh4a.R;
 
 import org.eclipse.egit.github.core.CodeSearchResult;
 import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.TextMatch;
+
+import java.util.List;
 
 public class CodeSearchAdapter extends RootAdapter<CodeSearchResult, CodeSearchAdapter.ViewHolder> {
     public CodeSearchAdapter(Context context) {
@@ -19,26 +26,57 @@ public class CodeSearchAdapter extends RootAdapter<CodeSearchResult, CodeSearchA
 
     @Override
     public ViewHolder onCreateViewHolder(LayoutInflater inflater, ViewGroup parent) {
-        View v = inflater.inflate(R.layout.row_two_line, parent, false);
+        View v = inflater.inflate(R.layout.row_code_search, parent, false);
         return new ViewHolder(v);
     }
 
     @Override
-    protected void onBindViewHolder(ViewHolder holder, CodeSearchResult item) {
-        Repository repo = item.getRepository();
-        holder.tvTitle.setText(item.getName());
-        holder.tvExtra.setText(mContext.getString(R.string.code_search_result_in,
-                repo.getOwner().getLogin(), repo.getName()));
+    protected void onBindViewHolder(ViewHolder holder, CodeSearchResult result) {
+        Repository repo = result.getRepository();
+        holder.tvTitle.setText(result.getName());
+        holder.tvRepo.setText(repo.getOwner().getLogin() + "/" + repo.getName());
+
+        List<TextMatch> matches = result.getTextMatches();
+        if (matches != null && !matches.isEmpty()) {
+            SpannableStringBuilder builder = new SpannableStringBuilder();
+
+            for (TextMatch match : matches) {
+                int pos = builder.length();
+                if (pos > 0) {
+                    builder.append("\n\n");
+                    builder.setSpan(new RelativeSizeSpan(0.5f), pos, pos + 2, 0);
+                    pos += 2;
+                }
+                builder.append(match.getFragment());
+
+                List<TextMatch.MatchItem> items = match.getMatches();
+                if (items != null) {
+                    for (TextMatch.MatchItem item : items) {
+                        int start = item.getStartPos();
+                        int end = item.getEdndPos();
+                        if (start >= 0 && end > start) {
+                            builder.setSpan(new StyleSpan(Typeface.BOLD), pos + start, pos + end, 0);
+                        }
+                    }
+                }
+            }
+            holder.tvMatches.setText(builder);
+            holder.tvMatches.setVisibility(View.VISIBLE);
+        } else {
+            holder.tvMatches.setVisibility(View.GONE);
+        }
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private ViewHolder(View view) {
             super(view);
             tvTitle = (TextView) view.findViewById(R.id.tv_title);
-            tvExtra = (TextView) view.findViewById(R.id.tv_extra);
+            tvRepo = (TextView) view.findViewById(R.id.tv_repo);
+            tvMatches = (TextView) view.findViewById(R.id.tv_matches);
         }
 
         private TextView tvTitle;
-        private TextView tvExtra;
+        private TextView tvRepo;
+        private TextView tvMatches;
     }
 }
--- src/com/gh4a/loader/CodeSearchLoader.java
+++ src/com/gh4a/loader/CodeSearchLoader.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.egit.github.core.CodeSearchResult;
+import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.RepositoryService;
 
 import android.content.Context;
 import android.text.TextUtils;
 
+import com.gh4a.DefaultClient;
 import com.gh4a.Gh4Application;
 
 public class CodeSearchLoader extends BaseLoader<List<CodeSearchResult>> {
@@ -21,12 +23,15 @@ public class CodeSearchLoader extends BaseLoader<List<CodeSearchResult>> {
 
     @Override
     public List<CodeSearchResult> doLoadInBackground() throws Exception {
+        Gh4Application app = (Gh4Application) getContext().getApplicationContext();
+        GitHubClient client = new DefaultClient("application/vnd.github.v3.text-match+json");
+        client.setOAuth2Token(app.getAuthToken());
+
         if (TextUtils.isEmpty(mQuery)) {
             return new ArrayList<>();
         }
 
-        RepositoryService repoService = (RepositoryService)
-                Gh4Application.get().getService(Gh4Application.REPO_SERVICE);
+        RepositoryService repoService = new RepositoryService(client);
         return repoService.searchCode(mQuery);
     }
 }
