slapperwan/gh4a

commit 4871dddeac30f7315e91461dc35e805d44ab9454

Refactor activity intent handling.
Instead of instantiating the intents at the caller's side as before,
give every activity a static intent factory method. This allows getting
rid of the extra name constants and at the same time serves as a
documentation of the input data consumed by the activity.

src/com/gh4a/fragment/RepositoryListFragment.java
@@ -27,7 +27,6 @@ import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 
-import com.gh4a.Constants;
 import com.gh4a.Gh4Application;
 import com.gh4a.R;
 import com.gh4a.adapter.RepositoryAdapter;
@@ -42,16 +41,16 @@ public class RepositoryListFragment extends PagedDataBaseFragment<Repository> {
     private String mSortOrder;
     private String mSortDirection;
 
-    public static RepositoryListFragment newInstance(String login, String userType,
+    public static RepositoryListFragment newInstance(String login, boolean isOrg,
             String repoType, String sortOrder, String sortDirection) {
         RepositoryListFragment f = new RepositoryListFragment();
 
         Bundle args = new Bundle();
-        args.putString(Constants.User.LOGIN, login);
-        args.putString(Constants.User.TYPE, userType);
-        args.putString(Constants.Repository.TYPE, repoType);
-        args.putString("sortOrder", sortOrder);
-        args.putString("sortDirection", sortDirection);
+        args.putString("user", login);
+        args.putBoolean("is_org", isOrg);
+        args.putString("repo_type", repoType);
+        args.putString("sort_order", sortOrder);
+        args.putString("sort_direction", sortDirection);
         f.setArguments(args);
 
         return f;
@@ -60,9 +59,9 @@ public class RepositoryListFragment extends PagedDataBaseFragment<Repository> {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mLogin = getArguments().getString(Constants.User.LOGIN);
-        mRepoType = getArguments().getString(Constants.Repository.TYPE);
-        mIsOrg = Constants.User.TYPE_ORG.equals(getArguments().getString(Constants.User.TYPE));
+        mLogin = getArguments().getString("user");
+        mRepoType = getArguments().getString("repo_type");
+        mIsOrg = getArguments().getBoolean("is_org");
         mSortOrder = getArguments().getString("sortOrder");
         mSortDirection = getArguments().getString("sortDirection");
     }

commit 8f37e23ba6cc9113c52bf15219f324281286890b

Fix repo list sorting.
Got broken in commit 4871dddeac30f7315e91461dc35e805d44ab9454.

src/com/gh4a/fragment/RepositoryListFragment.java
@@ -62,8 +62,8 @@ public class RepositoryListFragment extends PagedDataBaseFragment<Repository> {
         mLogin = getArguments().getString("user");
         mRepoType = getArguments().getString("repo_type");
         mIsOrg = getArguments().getBoolean("is_org");
-        mSortOrder = getArguments().getString("sortOrder");
-        mSortDirection = getArguments().getString("sortDirection");
+        mSortOrder = getArguments().getString("sort_order");
+        mSortDirection = getArguments().getString("sort_direction");
     }
 
     @Override