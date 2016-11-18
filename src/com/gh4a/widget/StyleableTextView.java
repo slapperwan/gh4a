fdb65c27259534f18d0e5202314657e033ffd6e8
Danny Baumann <dannybaumann@web.de>
2016-11-18 13:08:22 +0100

    Add workaround for a crash in TextView selection handling.
    See https://code.google.com/p/android/issues/detail?id=191430

--- src/com/gh4a/widget/StyleableTextView.java
+++ src/com/gh4a/widget/StyleableTextView.java
@@ -110,4 +110,17 @@ public class StyleableTextView extends TextView {
             setMovementMethod(UiUtils.CHECKING_LINK_METHOD);
         }
     }
+
+    // workaround for https://code.google.com/p/android/issues/detail?id=191430
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        int startSelection = getSelectionStart();
+        int endSelection = getSelectionEnd();
+        if (startSelection != endSelection && event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            final CharSequence text = getText();
+            setText(null);
+            setText(text);
+        }
+        return super.dispatchTouchEvent(event);
+    }
 }
