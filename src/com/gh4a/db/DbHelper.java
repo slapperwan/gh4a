e2810877091867fd3df74c68185d179950ed291e
Danny Baumann <dannybaumann@web.de>
2017-02-18 14:37:17 +0100

    Correctly update database.

--- src/com/gh4a/db/DbHelper.java
+++ src/com/gh4a/db/DbHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DbHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "gh4adb.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     static final String BOOKMARKS_TABLE = "bookmarks";
     static final String SUGGESTIONS_TABLE = "suggestions";
@@ -17,21 +17,32 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        createBookmarksTable(db);
+        createSuggestionsTable(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion < 2) {
+            createSuggestionsTable(db);
+        }
+    }
+
+    private void createBookmarksTable(SQLiteDatabase db) {
         db.execSQL("create table " + BOOKMARKS_TABLE + " ("
                 + "_id integer primary key autoincrement, "
                 + "name text not null, "
                 + "type integer not null, "
                 + "uri text not null, "
                 + "extra_data text);");
+    }
+
+    private void createSuggestionsTable(SQLiteDatabase db) {
         db.execSQL("create table " + SUGGESTIONS_TABLE + " ("
                 + "_id integer primary key autoincrement, "
                 + "type integer not null, "
                 + "suggestion text, "
                 + "date long, "
                 + "unique (type, suggestion) on conflict replace);");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
 }
