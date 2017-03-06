03deb351beeec38978c4037a538c5117178326bd
Tommy-Geenexus <roboter972@gmail.com>
2017-03-06 14:06:45 +0100

    Delete unnecessary import

--- src/com/gh4a/activities/GistViewerActivity.java
+++ src/com/gh4a/activities/GistViewerActivity.java
@@ -21,7 +21,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.Loader;
 import android.support.v7.app.ActionBar;
-import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
