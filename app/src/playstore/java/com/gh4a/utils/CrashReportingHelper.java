4ab67490eb1d49aab77212d001bc1844011435b0
thermatk <thermatk@thermatk.com>
2017-04-24 09:57:11 +0200

    final final

--- app/src/playstore/java/com/gh4a/utils/CrashReportingHelper.java
+++ app/src/playstore/java/com/gh4a/utils/CrashReportingHelper.java
@@ -6,29 +6,28 @@ import android.os.Build;
 import android.text.TextUtils;
 
 import com.crashlytics.android.Crashlytics;
-import com.gh4a.BuildConfig;
 
 import io.fabric.sdk.android.Fabric;
 
 public class CrashReportingHelper {
     private static final int MAX_TRACKED_URLS = 5;
-    private static int position = 0;
-    private static boolean hasCrashlytics;
+    private static int sNextUrlTrackingPosition = 0;
+    private static boolean sHasCrashlytics;
 
     public static void onCreate(Application app) {
         boolean isDebuggable = (app.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
-        hasCrashlytics = (!isDebuggable && !TextUtils.equals(Build.DEVICE, "sdk") && !BuildConfig.FOSS);
+        sHasCrashlytics = (!isDebuggable && !TextUtils.equals(Build.DEVICE, "sdk"));
 
-        if (hasCrashlytics) {
+        if (sHasCrashlytics) {
             Fabric.with(app, new Crashlytics());
         }
     }
     public static void trackVisitedUrl(Application app, String url) {
-        if (hasCrashlytics) {
-            Crashlytics.setString("github-url-" + position, url);
-            Crashlytics.setInt("last-url-position", position);
-            if (++position >= MAX_TRACKED_URLS) {
-                position = 0;
+        if (sHasCrashlytics) {
+            Crashlytics.setString("github-url-" + sNextUrlTrackingPosition, url);
+            Crashlytics.setInt("last-url-position", sNextUrlTrackingPosition);
+            if (++sNextUrlTrackingPosition >= MAX_TRACKED_URLS) {
+                sNextUrlTrackingPosition = 0;
             }
         }
     }
