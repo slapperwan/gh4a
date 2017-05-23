e5163d3fcd24cdcbdd349d822933df87940ec3bb
Tunous <lukus178@gmail.com>
2017-05-23 19:41:04 +0200

    Add notifications indicator

---
+++ app/src/main/java/com/gh4a/loader/HasNotificationsLoader.java
@@ -0,0 +1,26 @@
+package com.gh4a.loader;
+
+import android.content.Context;
+
+import com.gh4a.Gh4Application;
+
+import org.eclipse.egit.github.core.Notification;
+import org.eclipse.egit.github.core.client.PageIterator;
+import org.eclipse.egit.github.core.service.NotificationService;
+
+import java.io.IOException;
+
+public class HasNotificationsLoader extends BaseLoader<Boolean> {
+
+    public HasNotificationsLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    public Boolean doLoadInBackground() throws IOException {
+        NotificationService notificationService = (NotificationService)
+                Gh4Application.get().getService(Gh4Application.NOTIFICATION_SERVICE);
+        PageIterator<Notification> notifications = notificationService.pageNotifications(1);
+        return notifications.hasNext() && !notifications.next().isEmpty();
+    }
+}
