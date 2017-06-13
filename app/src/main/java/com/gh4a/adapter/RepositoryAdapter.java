slapperwan/gh4a

commit 76f9ebea89c90c3085052a397f7fe6a135af7f3b

Make repository attributes scrollable

app/src/main/res/layout/row_repo.xml
@@ -26,75 +26,81 @@
         android:textAppearance="?android:attr/textAppearanceSmall"
         tools:text="Short repository description" />
 
-    <LinearLayout
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:layout_marginTop="2dp"
-        android:orientation="horizontal">
+    <HorizontalScrollView
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content">
 
-        <com.gh4a.widget.StyleableTextView
-            android:id="@+id/tv_private"
+        <LinearLayout
             android:layout_width="wrap_content"
             android:layout_height="wrap_content"
-            android:layout_marginRight="16dp"
-            android:drawableLeft="?attr/privateSmallIcon"
-            android:drawablePadding="4dp"
-            android:text="@string/repo_type_private"
-            android:textAppearance="@style/TextAppearance.VerySmall.Bold"
-            android:visibility="gone"
-            tools:visibility="visible" />
+            android:layout_marginTop="2dp"
+            android:orientation="horizontal">
 
-        <com.gh4a.widget.StyleableTextView
-            android:id="@+id/tv_fork"
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginRight="16dp"
-            android:drawableLeft="?attr/forkSmallBoldIcon"
-            android:drawablePadding="4dp"
-            android:text="@string/fork"
-            android:textAppearance="@style/TextAppearance.VerySmall.Bold"
-            android:visibility="gone"
-            tools:visibility="visible" />
+            <com.gh4a.widget.StyleableTextView
+                android:id="@+id/tv_private"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:layout_marginRight="16dp"
+                android:drawableLeft="?attr/privateSmallIcon"
+                android:drawablePadding="4dp"
+                android:text="@string/repo_type_private"
+                android:textAppearance="@style/TextAppearance.VerySmall.Bold"
+                android:visibility="gone"
+                tools:visibility="visible" />
 
-        <com.gh4a.widget.StyleableTextView
-            android:id="@+id/tv_language"
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:drawableLeft="?attr/languageSmallIcon"
-            android:drawablePadding="4dp"
-            android:textAppearance="@style/TextAppearance.VerySmall"
-            tools:text="Language" />
+            <com.gh4a.widget.StyleableTextView
+                android:id="@+id/tv_fork"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:layout_marginRight="16dp"
+                android:drawableLeft="?attr/forkSmallBoldIcon"
+                android:drawablePadding="4dp"
+                android:text="@string/fork"
+                android:textAppearance="@style/TextAppearance.VerySmall.Bold"
+                android:visibility="gone"
+                tools:visibility="visible" />
 
-        <com.gh4a.widget.StyleableTextView
-            android:id="@+id/tv_forks"
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginLeft="16dp"
-            android:drawableLeft="?attr/forkSmallIcon"
-            android:drawablePadding="4dp"
-            android:textAppearance="@style/TextAppearance.VerySmall"
-            tools:text="0" />
+            <com.gh4a.widget.StyleableTextView
+                android:id="@+id/tv_language"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:drawableLeft="?attr/languageSmallIcon"
+                android:drawablePadding="4dp"
+                android:textAppearance="@style/TextAppearance.VerySmall"
+                tools:text="Language" />
 
-        <com.gh4a.widget.StyleableTextView
-            android:id="@+id/tv_stars"
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginLeft="16dp"
-            android:drawableLeft="?attr/starSmallIcon"
-            android:drawablePadding="4dp"
-            android:textAppearance="@style/TextAppearance.VerySmall"
-            tools:text="0" />
+            <com.gh4a.widget.StyleableTextView
+                android:id="@+id/tv_forks"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:layout_marginLeft="16dp"
+                android:drawableLeft="?attr/forkSmallIcon"
+                android:drawablePadding="4dp"
+                android:textAppearance="@style/TextAppearance.VerySmall"
+                tools:text="0" />
 
-        <com.gh4a.widget.StyleableTextView
-            android:id="@+id/tv_size"
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginLeft="16dp"
-            android:drawableLeft="?attr/sizeSmallIcon"
-            android:drawablePadding="4dp"
-            android:textAppearance="@style/TextAppearance.VerySmall"
-            tools:text="0" />
+            <com.gh4a.widget.StyleableTextView
+                android:id="@+id/tv_stars"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:layout_marginLeft="16dp"
+                android:drawableLeft="?attr/starSmallIcon"
+                android:drawablePadding="4dp"
+                android:textAppearance="@style/TextAppearance.VerySmall"
+                tools:text="0" />
 
-    </LinearLayout>
+            <com.gh4a.widget.StyleableTextView
+                android:id="@+id/tv_size"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:layout_marginLeft="16dp"
+                android:drawableLeft="?attr/sizeSmallIcon"
+                android:drawablePadding="4dp"
+                android:textAppearance="@style/TextAppearance.VerySmall"
+                tools:text="0" />
+
+        </LinearLayout>
+
+    </HorizontalScrollView>
 
 </LinearLayout>

commit c25e56db4c88ecb99fa8f03621a88aa41dcc9a92

Fix being unable to open repo when clicking on attributes
Fixes an issue introduced in 76f9ebea89c90c3085052a397f7fe6a135af7f3b where when clicking on repository attributes no longer opened the repository.

app/src/main/java/com/gh4a/adapter/RepositoryAdapter.java
@@ -19,6 +19,7 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.Formatter;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filterable;
@@ -72,7 +73,8 @@ public class RepositoryAdapter extends RootAdapter<Repository, RepositoryAdapter
         return name.contains(lcFilter);
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,
+            View.OnTouchListener {
         private ViewHolder(View view) {
             super(view);
             tvTitle = (TextView) view.findViewById(R.id.tv_title);
@@ -83,6 +85,9 @@ public class RepositoryAdapter extends RootAdapter<Repository, RepositoryAdapter
             tvSize = (TextView) view.findViewById(R.id.tv_size);
             tvPrivate = (TextView) view.findViewById(R.id.tv_private);
             tvFork = (TextView) view.findViewById(R.id.tv_fork);
+
+            view.findViewById(R.id.attributes).setOnClickListener(this);
+            view.findViewById(R.id.scrollView).setOnTouchListener(this);
         }
 
         private final TextView tvTitle;
@@ -93,5 +98,17 @@ public class RepositoryAdapter extends RootAdapter<Repository, RepositoryAdapter
         private final TextView tvSize;
         private final TextView tvPrivate;
         private final TextView tvFork;
+
+        @Override
+        public void onClick(View v) {
+            // Workaround to make it possible to open repositories when clicking inside of
+            // attributes ScrollView
+            itemView.performClick();
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return false;
+        }
     }
 }

app/src/main/res/layout/row_repo.xml
@@ -27,10 +27,13 @@
         tools:text="Short repository description" />
 
     <HorizontalScrollView
+        android:id="@+id/scrollView"
+        android:clickable="false"
         android:layout_width="match_parent"
         android:layout_height="wrap_content">
 
         <LinearLayout
+            android:id="@+id/attributes"
             android:layout_width="wrap_content"
             android:layout_height="wrap_content"
             android:layout_marginTop="2dp"