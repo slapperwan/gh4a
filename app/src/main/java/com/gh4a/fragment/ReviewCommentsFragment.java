428f0a353172d7c8e1dd592a36fb07fad6707684
Tunous <lukus178@gmail.com>
2017-06-14 14:33:25 +0200

    Add comment deletion and editing to review comments activity

--- app/src/main/java/com/gh4a/fragment/ReviewCommentsFragment.java
+++ app/src/main/java/com/gh4a/fragment/ReviewCommentsFragment.java
@@ -1,7 +1,11 @@
 package com.gh4a.fragment;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 
@@ -9,12 +13,15 @@ import com.gh4a.BaseActivity;
 import com.gh4a.Gh4Application;
 import com.gh4a.ProgressDialogTask;
 import com.gh4a.R;
+import com.gh4a.activities.EditIssueCommentActivity;
+import com.gh4a.activities.EditPullRequestCommentActivity;
 import com.gh4a.adapter.timeline.TimelineItemAdapter;
 import com.gh4a.loader.TimelineItem;
 
 import org.eclipse.egit.github.core.Comment;
 import org.eclipse.egit.github.core.CommitComment;
 import org.eclipse.egit.github.core.RepositoryId;
+import org.eclipse.egit.github.core.service.IssueService;
 import org.eclipse.egit.github.core.service.PullRequestService;
 
 import java.io.IOException;
@@ -24,6 +31,8 @@ import java.util.List;
 
 public class ReviewCommentsFragment extends LoadingListFragmentBase
         implements TimelineItemAdapter.OnCommentAction {
+
+    private static final int REQUEST_EDIT = 1000;
 
     private TimelineItemAdapter mAdapter;
 
@@ -79,7 +88,28 @@ public class ReviewCommentsFragment extends LoadingListFragmentBase
     }
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_EDIT) {
+            if (resultCode == Activity.RESULT_OK) {
+                reloadComments(true);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
     public void onRefresh() {
+        // TODO
+    }
+
+    private void reloadComments( boolean alsoClearCaches) {
+        if (mAdapter != null && !alsoClearCaches) {
+            // Don't clear adapter's cache, we're only interested in the new event
+            mAdapter.suppressCacheClearOnNextClear();
+        }
+
+        onRefresh();
     }
 
     @Override
@@ -89,12 +119,30 @@ public class ReviewCommentsFragment extends LoadingListFragmentBase
 
     @Override
     public void editComment(Comment comment) {
+        Intent intent;
+        if (comment instanceof CommitComment) {
+            intent = EditPullRequestCommentActivity.makeIntent(getActivity(), mRepoOwner, mRepoName,
+                    mIssueNumber, (CommitComment) comment);
+        } else {
+            intent = EditIssueCommentActivity.makeIntent(getActivity(), mRepoOwner, mRepoName,
+                    mIssueNumber, comment);
+        }
 
+        startActivityForResult(intent, REQUEST_EDIT);
     }
 
     @Override
-    public void deleteComment(Comment comment) {
-
+    public void deleteComment(final Comment comment) {
+        new AlertDialog.Builder(getActivity())
+                .setMessage(R.string.delete_comment_message)
+                .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new DeleteCommentTask(getBaseActivity(), comment).schedule();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
     }
 
     @Override
@@ -141,12 +189,53 @@ public class ReviewCommentsFragment extends LoadingListFragmentBase
 
         @Override
         protected void onSuccess(CommitComment result) {
-            // TODO: Refresh comments
+            reloadComments(true);
         }
 
         @Override
         protected String getErrorMessage() {
             return getContext().getString(R.string.issue_error_comment);
+        }
+    }
+
+    private class DeleteCommentTask extends ProgressDialogTask<Void> {
+        private final Comment mComment;
+
+        public DeleteCommentTask(BaseActivity activity, Comment comment) {
+            super(activity, R.string.deleting_msg);
+            mComment = comment;
+        }
+
+        @Override
+        protected ProgressDialogTask<Void> clone() {
+            return new DeleteCommentTask(getBaseActivity(), mComment);
+        }
+
+        @Override
+        protected Void run() throws Exception {
+            RepositoryId repoId = new RepositoryId(mRepoOwner, mRepoName);
+            Gh4Application app = Gh4Application.get();
+
+            if (mComment instanceof CommitComment) {
+                PullRequestService pullService =
+                        (PullRequestService) app.getService(Gh4Application.PULL_SERVICE);
+                pullService.deleteComment(repoId, mComment.getId());
+            } else {
+                IssueService issueService =
+                        (IssueService) app.getService(Gh4Application.ISSUE_SERVICE);
+                issueService.deleteComment(repoId, mComment.getId());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onSuccess(Void result) {
+            reloadComments(false);
+        }
+
+        @Override
+        protected String getErrorMessage() {
+            return getContext().getString(R.string.error_delete_comment);
         }
     }
 }
