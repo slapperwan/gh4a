54308454e860defe59c66094244f5c30b51cb6d0
Łukasz Rutkowski <lukus178@gmail.com>
2017-08-29 16:47:02 +0200

    Fix FAB not re-appearing when scrolling back
    As of support library version 25.1.0 GONE views no longer receive nested scroll events. To workaround this change the FAB is now set to INVISIBLE when hiding instead of GONE.
    
    Closes #673

--- app/src/main/java/com/gh4a/widget/ScrollAwareFloatingActionButtonBehavior.java
+++ app/src/main/java/com/gh4a/widget/ScrollAwareFloatingActionButtonBehavior.java
@@ -1,6 +1,7 @@
 package com.gh4a.widget;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
@@ -11,28 +12,39 @@ import com.gh4a.R;
 
 @SuppressWarnings("unused")
 public class ScrollAwareFloatingActionButtonBehavior extends FloatingActionButton.Behavior {
-    public ScrollAwareFloatingActionButtonBehavior(Context context, AttributeSet attrs) {
-        super();
-    }
-
-    @Override
-    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
-            FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
-        if (target.getTag(R.id.FloatingActionButtonScrollEnabled) == null) {
-            return super.onStartNestedScroll(coordinatorLayout, child,
-                    directTargetChild, target, nestedScrollAxes);
+    private FloatingActionButton.OnVisibilityChangedListener mVisibilityChangedListener =
+            new FloatingActionButton.OnVisibilityChangedListener() {
+        @Override
+        public void onHidden(FloatingActionButton fab) {
+            super.onHidden(fab);
+            fab.setVisibility(View.INVISIBLE);
         }
-        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    };
+
+    public ScrollAwareFloatingActionButtonBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
 
     @Override
-    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
-            View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+            @NonNull FloatingActionButton child, @NonNull View directTargetChild,
+            @NonNull View target, int axes, int type) {
+        if (target.getTag(R.id.FloatingActionButtonScrollEnabled) == null) {
+            return super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
+                    axes, type);
+        }
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+            @NonNull FloatingActionButton child, @NonNull View target, int dxConsumed,
+            int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
-                dyUnconsumed);
+                dyUnconsumed, type);
 
         if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
-            child.hide();
+            child.hide(mVisibilityChangedListener);
         } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
             child.show();
         }
