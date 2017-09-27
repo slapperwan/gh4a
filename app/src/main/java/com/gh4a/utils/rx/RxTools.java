f045d2d7887fa589249d8c77954f01e07abcf378
Mattia PRIMAVERA <mprimavera@aedmap.com>
2017-09-27 20:34:36 +0200

    RxTools cast fixes

--- app/src/main/java/com/gh4a/utils/rx/RxTools.java
+++ app/src/main/java/com/gh4a/utils/rx/RxTools.java
@@ -2,8 +2,6 @@ package com.gh4a.utils.rx;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableTransformer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
@@ -13,15 +11,14 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import com.gh4a.BasePagerActivity;
+import com.gh4a.BaseActivity;
 import com.gh4a.R;
 import com.gh4a.utils.UiUtils;
 import com.philosophicalhacker.lib.RxLoader;
-
 import java.util.concurrent.Callable;
 
 public class RxTools {
-    public static Observable runCallable(Callable call, Activity activity, int loaderId, boolean refresh) {
+    public static Observable runCallable(Callable call, BaseActivity activity, int loaderId, boolean refresh) {
         return Observable.fromCallable(call)
                 .compose(RxTools.handle(activity, loaderId, refresh));
     }
@@ -35,51 +32,40 @@ public class RxTools {
         };
     }
 
-    public static <T> ObservableTransformer<T, T> handle(Activity activity, int id, boolean refresh) {
+    public static <T> ObservableTransformer<T, T> handle(BaseActivity activity, int id, boolean refresh) {
         return observable -> {
-            final RxLoader rxLoader = new RxLoader(activity, ((AppCompatActivity)activity).getSupportLoaderManager());
+            final RxLoader rxLoader = new RxLoader(activity, activity.getSupportLoaderManager());
 
-            if(refresh) {
+            if (refresh) {
                 return observable
-                        .compose(handleError(activity))
-                        .compose(rxLoader.makeObservableTransformer(id, refresh));
-            } else return observable.compose(handle(activity, id));
+                    .compose(handleError(activity))
+                    .compose(rxLoader.makeObservableTransformer(id, refresh));
+            } else {
+                return observable.compose(handle(activity, id));
+            }
         };
     }
 
     // Error handler showing SnackBar
-    public static <T> ObservableTransformer<T, T> onErrorSnackbar(Activity activity, int id, View rootLayout, String errorMessage, int messageRes) {
+    public static <T> ObservableTransformer<T, T> onErrorSnackbar(BaseActivity activity, int id, View rootLayout, String errorMessage, int messageRes) {
         return observable -> {
-            final RxLoader rxLoader = new RxLoader(activity, ((AppCompatActivity)activity).getSupportLoaderManager());
+            final RxLoader rxLoader = new RxLoader(activity, activity.getSupportLoaderManager());
             return observable
-                    .compose(rxLoader.makeObservableTransformer(id))
-                    .doOnSubscribe(disposable -> showProgressDialog(activity, messageRes))
-                    .doOnTerminate(() -> dismissProgressDialog())
-                    .doOnError(error -> {
-                        Snackbar.make(rootLayout, errorMessage, Snackbar.LENGTH_LONG)
-                                .setAction(R.string.retry, (View.OnClickListener) activity)
-                                .show();
-                    });
+                .compose(rxLoader.makeObservableTransformer(id))
+                .doOnSubscribe(disposable -> showProgressDialog(activity, messageRes))
+                .doOnTerminate(() -> dismissProgressDialog())
+                .doOnError(error -> {
+                    Snackbar.make(rootLayout, errorMessage, Snackbar.LENGTH_LONG)
+                            .setAction(R.string.retry, (View.OnClickListener) activity)
+                            .show();
+                });
         };
     }
 
-    public static <T> ObservableTransformer<T, T> handleError(Activity activity) {
+    public static <T> ObservableTransformer<T, T> handleError(BaseActivity activity) {
         return observable -> observable
-                .doOnError(error -> {
-                    if (activity instanceof BasePagerActivity) {
-                        BasePagerActivity act = (BasePagerActivity) activity;
-                        act.setErrorViewVisibility(true);
-                    } else {
-                        // other cases
-                    }
-                });
+            .doOnError(error -> activity.setErrorViewVisibility(true));
     }
-
-    public static <T> ObservableTransformer<T, T> applySchedulers() {
-        return observable -> ((Observable)observable).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
-    }
-
 
     public static ProgressDialogFragment mProgressDialogFragment;
 
@@ -99,11 +85,11 @@ public class RxTools {
         mProgressDialogFragment = null;
     }
 
-    public static void showProgressDialog(Activity activity, int messageRes) {
+    public static void showProgressDialog(BaseActivity activity, int messageRes) {
         Bundle args = new Bundle();
         args.putInt("message_res", messageRes);
         mProgressDialogFragment = new ProgressDialogFragment();
         mProgressDialogFragment.setArguments(args);
-        mProgressDialogFragment.show(((FragmentActivity)activity).getSupportFragmentManager(), "progressdialog");
+        mProgressDialogFragment.show(activity.getSupportFragmentManager(), "progressdialog");
     }
 }
