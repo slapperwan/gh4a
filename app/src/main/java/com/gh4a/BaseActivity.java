56ebab7099b039634b342aa50d3b0c361e49fde6
Danny Baumann <dannybaumann@web.de>
2017-10-19 14:05:55 +0200

    Fix login/logout handling.

--- app/src/main/java/com/gh4a/BaseActivity.java
+++ app/src/main/java/com/gh4a/BaseActivity.java
@@ -72,6 +72,7 @@ import com.gh4a.widget.ToggleableAppBarLayoutBehavior;
 import com.meisolsson.githubsdk.model.ClientErrorResponse;
 import com.philosophicalhacker.lib.RxLoader;
 
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -165,20 +166,14 @@ public abstract class BaseActivity extends AppCompatActivity implements
         return null;
     }
 
-    public void handleAuthFailureDuringLoad() {
-        Gh4Application.get().logout();
-        Snackbar.make(mCoordinatorLayout, R.string.load_auth_failure_notice, Snackbar.LENGTH_INDEFINITE)
-                .setAction(R.string.login, new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        goToToplevelActivity();
-                    }
-                })
-                .show();
-    }
-
     public void handleLoadFailure(Throwable e) {
-        // FIXME: handle auth error by integrating handleAuthFailureDuringLoad
+        boolean isAuthError = e instanceof ApiRequestException
+                && ((ApiRequestException) e).getStatus() == HttpURLConnection.HTTP_UNAUTHORIZED;
+        if (isAuthError) {
+            Snackbar.make(mCoordinatorLayout, R.string.load_auth_failure_notice, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.login, v -> goToToplevelActivity())
+                    .show();
+        }
         Log.d(Gh4Application.LOG_TAG, "Loading data failed", e);
         setErrorViewVisibility(true, e);
     }
