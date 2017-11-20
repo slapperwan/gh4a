c0d9d062318e281b417a0c369f93f342346fcb4b
Danny Baumann <dannybaumann@web.de>
2017-11-20 12:13:11 +0100

    Clean up Crashlytics handling.
    Avoid unnecessary indirection, and don't synchronize if not needed.

--- app/src/foss/java/com/gh4a/utils/CrashReportingHelper.java
+++ app/src/foss/java/com/gh4a/utils/CrashReportingHelper.java
@@ -1,11 +1,11 @@
 package com.gh4a.utils;
 
-import android.app.Application;
+import android.content.Context;
 
 public class CrashReportingHelper {
 
-    public static void onCreate(Application app) {
+    public static void onCreate(Context context) {
     }
-    public static void trackVisitedUrl(Application app, String url) {
+    public static void trackVisitedUrl(String url) {
     }
 }
