slapperwan/gh4a

commit 74c9b80d6a4d5fa3300f1d7c73966c62f9c7c26f

Fix crash when commit author data is missing
Closes #829

app/src/main/java/com/gh4a/adapter/CommitAdapter.java
@@ -19,6 +19,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,12 +52,13 @@ public class CommitAdapter extends RootAdapter<Commit, CommitAdapter.ViewHolder>
     @Override
     public void onBindViewHolder(ViewHolder holder, Commit commit) {
         User author = commit.author();
-        if (author != null) {
+        if (author != null && !TextUtils.isEmpty(author.name())) {
             AvatarHandler.assignAvatar(holder.ivGravatar, author);
         } else {
             GitUser commitAuthor = commit.commit().author();
+            String userName = commitAuthor != null ? commitAuthor.name() : null;
             String email = commitAuthor != null ? commitAuthor.email() : null;
-            holder.ivGravatar.setImageDrawable(new AvatarHandler.DefaultAvatarDrawable(null, email));
+            holder.ivGravatar.setImageDrawable(new AvatarHandler.DefaultAvatarDrawable(userName, email));
         }
         holder.ivGravatar.setTag(commit);
 

app/src/main/java/com/gh4a/utils/ApiHelpers.java
@@ -51,11 +51,13 @@ public class ApiHelpers {
 
     //RepositoryCommit
     public static String getAuthorName(Context context, Commit commit) {
-        if (commit.author() != null) {
-            return commit.author().login();
+        User author = commit.author();
+        if (author != null && !TextUtils.isEmpty(author.login())) {
+            return author.login();
         }
-        if (commit.commit().author() != null) {
-            return commit.commit().author().name();
+        GitUser commitAuthor = commit.commit().author();
+        if (commitAuthor != null && !TextUtils.isEmpty(commitAuthor.name())) {
+            return commitAuthor.name();
         }
         return context.getString(R.string.unknown);
     }

app/src/main/java/com/gh4a/utils/AvatarHandler.java
@@ -92,7 +92,7 @@ public class AvatarHandler {
     };
 
     public static void assignAvatar(ImageView view, User user) {
-        if (user == null) {
+        if (user == null || user.id() == null) {
             assignAvatar(view, null, 0, null);
             return;
         }

commit f0f9491375076983d58c31adc136856f2e98778a

Fix typo causing avatars to not load in CommitListFragment
Introduced in 74c9b80d6a4d5fa3300f1d7c73966c62f9c7c26f

app/src/main/java/com/gh4a/adapter/CommitAdapter.java
@@ -52,7 +52,7 @@ public class CommitAdapter extends RootAdapter<Commit, CommitAdapter.ViewHolder>
     @Override
     public void onBindViewHolder(ViewHolder holder, Commit commit) {
         User author = commit.author();
-        if (author != null && !TextUtils.isEmpty(author.name())) {
+        if (author != null && !TextUtils.isEmpty(author.login())) {
             AvatarHandler.assignAvatar(holder.ivGravatar, author);
         } else {
             GitUser commitAuthor = commit.commit().author();