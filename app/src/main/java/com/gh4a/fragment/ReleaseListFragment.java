294457739429cc5a84a9a28ee742f3b94e3c6848
Fs00 <francescosaltori@gmail.com>
2021-09-13 21:24:54 +0200

    Sort releases by publishing date manually
    Works around an API misbehavior

--- app/src/main/java/com/gh4a/fragment/ReleaseListFragment.java
+++ app/src/main/java/com/gh4a/fragment/ReleaseListFragment.java
@@ -13,13 +13,22 @@ import com.meisolsson.githubsdk.model.Page;
 import com.meisolsson.githubsdk.model.Release;
 import com.meisolsson.githubsdk.service.repositories.RepositoryReleaseService;
 
+import java.util.Collections;
+import java.util.Comparator;
+
 import io.reactivex.Single;
 import retrofit2.Response;
+
+import static java.util.Comparator.reverseOrder;
+import static java.util.Comparator.nullsFirst;
 
 public class ReleaseListFragment extends PagedDataBaseFragment<Release> implements
         RootAdapter.OnItemClickListener<Release> {
     private String mUserLogin;
     private String mRepoName;
+
+    private static final Comparator<Release> MOST_RECENT_RELEASES_AND_DRAFTS_FIRST =
+            Comparator.comparing(Release::publishedAt, nullsFirst(reverseOrder()));
 
     public static ReleaseListFragment newInstance(String owner, String repo) {
         ReleaseListFragment f = new ReleaseListFragment();
@@ -39,9 +48,16 @@ public class ReleaseListFragment extends PagedDataBaseFragment<Release> implemen
 
     @Override
     protected Single<Response<Page<Release>>> loadPage(int page, boolean bypassCache) {
-        final RepositoryReleaseService service =
-                ServiceFactory.get(RepositoryReleaseService.class, bypassCache);
-        return service.getReleases(mUserLogin, mRepoName, page);
+        final RepositoryReleaseService service = ServiceFactory.get(RepositoryReleaseService.class, bypassCache);
+        return service.getReleases(mUserLogin, mRepoName, page)
+                // Sometimes the API returns releases in a slightly wrong order (see TeamNewPipe/NewPipe repo
+                // for an example), so we need to fix the sorting locally
+                .map(response -> {
+                    if (response.body() != null) {
+                        Collections.sort(response.body().items(), MOST_RECENT_RELEASES_AND_DRAFTS_FIRST);
+                    }
+                    return response;
+                });
     }
 
     @Override
