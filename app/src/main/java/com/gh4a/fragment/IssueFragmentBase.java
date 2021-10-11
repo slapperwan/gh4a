c4a2f5ccf1802e9e086a3f41f6062096327531b6
Fs00 <francescosaltori@gmail.com>
2021-10-11 09:22:53 +0200

    Display draft/ready for review events in PR conversation

--- app/src/main/java/com/gh4a/fragment/IssueFragmentBase.java
+++ app/src/main/java/com/gh4a/fragment/IssueFragmentBase.java
@@ -84,7 +84,8 @@ public abstract class IssueFragmentBase extends ListDataBaseFragment<TimelineIte
             IssueEventType.Labeled, IssueEventType.Unlabeled, IssueEventType.Locked,
             IssueEventType.Unlocked, IssueEventType.Milestoned, IssueEventType.Demilestoned,
             IssueEventType.Renamed, IssueEventType.HeadRefDeleted, IssueEventType.HeadRefRestored,
-            IssueEventType.ReviewRequested, IssueEventType.ReviewRequestRemoved
+            IssueEventType.ReviewRequested, IssueEventType.ReviewRequestRemoved,
+            IssueEventType.ConvertToDraft, IssueEventType.ReadyForReview
     );
 
     protected View mListHeaderView;
