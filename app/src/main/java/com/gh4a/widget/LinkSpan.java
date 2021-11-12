7d3f4dd4a69f94c155bee9c4e99ccdc41f5bcce2
Fs00 <francescosaltori@gmail.com>
2021-11-12 13:41:23 +0100

    Centralize link handling behavior for TextViews and WebViews

--- app/src/main/java/com/gh4a/widget/LinkSpan.java
+++ app/src/main/java/com/gh4a/widget/LinkSpan.java
@@ -5,8 +5,7 @@ import android.text.style.ClickableSpan;
 import android.view.View;
 
 import com.gh4a.BaseActivity;
-import com.gh4a.resolver.LinkParser;
-import com.gh4a.utils.IntentUtils;
+import com.gh4a.resolver.LinkClickHandler;
 
 import androidx.annotation.NonNull;
 
@@ -21,15 +20,6 @@ public class LinkSpan extends ClickableSpan {
     public void onClick(@NonNull View widget) {
         Uri clickedUri = Uri.parse(mUrl);
         BaseActivity activity = (BaseActivity) widget.getContext();
-        LinkParser.ParseResult result = LinkParser.parseUri(activity, clickedUri, null);
-
-        if (result == null) {
-            IntentUtils.openInCustomTabOrBrowser(activity, clickedUri, activity.getCurrentHeaderColor());
-        } else if (result.intent != null) {
-            activity.startActivity(result.intent);
-        } else if (result.loadTask != null) {
-            result.loadTask.setOpenUnresolvedUriInCustomTab(activity.getCurrentHeaderColor());
-            result.loadTask.execute();
-        }
+        new LinkClickHandler(activity).handleClick(clickedUri);
     }
 }
