8bfcbd2bcf319696a53ab0578195e92ee7b3b35c
Fs00 <francescosaltori@gmail.com>
2022-03-21 13:25:04 +0100

    Fix listing starred repos of other users

--- app/src/main/java/com/gh4a/fragment/RepositoryListContainerFragment.java
+++ app/src/main/java/com/gh4a/fragment/RepositoryListContainerFragment.java
@@ -192,6 +192,9 @@ public class RepositoryListContainerFragment extends Fragment implements
         }
 
         switch (mFilterType) {
+            case "starred":
+                mMainFragment = StarredRepositoryListFragment.newInstance(mUserLogin);
+                break;
             case "watched":
                 mMainFragment = WatchedRepositoryListFragment.newInstance(mUserLogin);
                 break;
@@ -365,6 +368,7 @@ public class RepositoryListContainerFragment extends Fragment implements
             FILTER_LOOKUP.put(R.id.filter_type_sources, "sources");
             FILTER_LOOKUP.put(R.id.filter_type_forks, "forks");
             FILTER_LOOKUP.put(R.id.filter_type_watched, "watched");
+            FILTER_LOOKUP.put(R.id.filter_type_starred, "starred");
         }
 
         public static FilterDrawerHelper create(String userLogin, boolean isOrg) {
@@ -427,7 +431,7 @@ public class RepositoryListContainerFragment extends Fragment implements
         }
 
         public int getMenuResId() {
-            return TextUtils.equals(mFilterType, "watched") ? 0 : R.menu.repo_sort;
+            return "watched".equals(mFilterType) || "starred".equals(mFilterType) ? 0 : R.menu.repo_sort;
         }
 
         public void selectSortType(Menu menu, String order, String direction,
