c03ff979d0847672b36fef0c78cc2f635c6542bd
Fs00 <francescosaltori@gmail.com>
2022-07-14 21:42:20 +0200

    Fix marking notifications as read

--- app/src/main/java/com/gh4a/fragment/NotificationListFragment.java
+++ app/src/main/java/com/gh4a/fragment/NotificationListFragment.java
@@ -17,7 +17,6 @@ import com.gh4a.ServiceFactory;
 import com.gh4a.activities.RepositoryActivity;
 import com.gh4a.adapter.NotificationAdapter;
 import com.gh4a.adapter.RootAdapter;
-import com.gh4a.resolver.LinkParser;
 import com.gh4a.worker.NotificationsWorker;
 import com.gh4a.model.NotificationHolder;
 import com.gh4a.resolver.BrowseFilter;
@@ -138,26 +137,15 @@ public class NotificationListFragment extends LoadingListFragmentBase implements
             String url = subject.url();
             if (url != null) {
                 Uri uri = ApiHelpers.normalizeUri(Uri.parse(url));
-                IntentUtils.InitialCommentMarker initialComment =
-                        new IntentUtils.InitialCommentMarker(item.notification.lastReadAt());
-                LinkParser.ParseResult result = LinkParser.parseUri(getActivity(), uri, initialComment);
-                if (result != null && result.intent != null) {
-                    intent = result.intent;
-                } else {
-                    // We either can't handle the URL ourselves or need to resolve it
-                    // asynchronously. Pass it to the BrowseFilter for resolving or redirection
-                    intent = BrowseFilter.makeRedirectionIntent(getActivity(), uri, initialComment);
-                    // Only auto-mark the notification as read if we can handle the URL ourselves
-                    if (result != null) {
-                        markAsRead(null, item.notification);
-                    }
-                }
+                intent = BrowseFilter.makeRedirectionIntent(getActivity(), uri,
+                        new IntentUtils.InitialCommentMarker(item.notification.lastReadAt()));
             } else {
                 intent = null;
             }
         }
 
         if (intent != null) {
+            markAsRead(null, item.notification);
             startActivity(intent);
         }
     }
