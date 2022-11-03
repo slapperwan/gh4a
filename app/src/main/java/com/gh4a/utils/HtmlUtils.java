c0ba6e09e519bd3c9cdf27531c5823b508509522
Fs00 <francescosaltori@gmail.com>
2022-11-03 18:05:43 +0100

    Treat <details> and <summary> tags as block elements

--- app/src/main/java/com/gh4a/utils/HtmlUtils.java
+++ app/src/main/java/com/gh4a/utils/HtmlUtils.java
@@ -429,8 +429,10 @@ public class HtmlUtils {
                     mSpannableStringBuilder.setSpan(span, mSpannableStringBuilder.length() - 2,
                             mSpannableStringBuilder.length() - 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                 }
-            } else if (tag.equalsIgnoreCase("div")) {
+            } else if (tag.equalsIgnoreCase("div") || tag.equalsIgnoreCase("details")) {
                 startBlockElement(attributes);
+            } else if (tag.equalsIgnoreCase("summary")) {
+                startBlockElement(attributes, 1);
             } else if (tag.equalsIgnoreCase("span")) {
                 startCssStyle(attributes);
             } else if (tag.equalsIgnoreCase("hr")) {
@@ -518,7 +520,9 @@ public class HtmlUtils {
                 end(List.class);
             } else if (tag.equalsIgnoreCase("li")) {
                 endLi();
-            } else if (tag.equalsIgnoreCase("div")) {
+            } else if (tag.equalsIgnoreCase("div") ||
+                       tag.equalsIgnoreCase("details") ||
+                       tag.equalsIgnoreCase("summary")) {
                 endBlockElement();
             } else if (tag.equalsIgnoreCase("span")) {
                 endCssStyle();
