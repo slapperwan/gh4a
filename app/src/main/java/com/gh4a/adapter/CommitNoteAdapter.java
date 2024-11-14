0acc437138cffcbe6e31677eff1de4793af3a5c1
Fs00 <francescosaltori@gmail.com>
2024-11-14 18:55:34 +0100

    Fix UI glitch when toggling a reaction on a commit comment

--- app/src/main/java/com/gh4a/adapter/CommitNoteAdapter.java
+++ app/src/main/java/com/gh4a/adapter/CommitNoteAdapter.java
@@ -239,7 +239,6 @@ public class CommitNoteAdapter extends RootAdapter<GitComment, CommitNoteAdapter
         if (holder.mReactionMenuHelper != null) {
             holder.mReactionMenuHelper.update();
         }
-        notifyItemChanged(holder.getBindingAdapterPosition());
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder implements
